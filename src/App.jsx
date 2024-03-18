1db528b350cd018ef9ddbed72598e517579bb73a
Jerome Encinares Talavera <139388162+encinares16@users.noreply.github.com>
2024-03-18 15:04:47 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
             const result = await axios.get(`${pokemon.url}`)
             setPokeData(state=>{ state=[...state, result.data] 
                 state.sort((a, b) => a.id > b.id ? 1 : -1)
-                // state.sort((a, b) => a.name > b.name ? 1:-1)
+                // state.sort((a, b) => a.name > b.name)
                 return state;
             })
         })   
